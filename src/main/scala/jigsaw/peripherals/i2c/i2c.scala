aca60785c9f178aac401f03a4d5a5b377d6ba628
shahzaibk23 <shahzaibceo@gmail.com>
2024-08-31 20:06:03 +0500

    update i2c w. new abstractions

--- src/main/scala/jigsaw/peripherals/i2c/i2c.scala
+++ src/main/scala/jigsaw/peripherals/i2c/i2c.scala
@@ -1,20 +1,26 @@
 package jigsaw.peripherals.i2c
 import chisel3._
 import chisel3.util._
+
+import jigsaw.peripherals.common.{AbstractDevice, AbstractDeviceIO}
 import caravan.bus.common.{BusConfig, AbstrRequest, AbstrResponse}
 
-class i2c(val req:AbstrRequest, val rsp:AbstrResponse)(implicit val config:BusConfig) extends Module {
-    val io = IO(new Bundle{
-        val request = Flipped(Decoupled(req))   // req aaygi
-        val response = Decoupled(rsp)           // resp jaayga
-        val cio_i2c_sda_in = Input(Bool())
+class I2cIO[A <: AbstrRequest, B <: AbstrResponse]
+        (gen: A, gen1: B) extends AbstractDeviceIO[A,B]{
+    val req = Flipped(Decoupled(gen))   // req aaygi
+    val rsp = Decoupled(gen1)           // resp jaayga
+    val cio_i2c_sda_in = Input(Bool())
 
-        val cio_i2c_sda = Output(Bool())
-        val cio_i2c_scl = Output(Bool())
-        val cio_i2c_intr = Output(Bool())
-    })
+    val cio_i2c_sda = Output(Bool())
+    val cio_i2c_scl = Output(Bool())
+    val cio_i2c_intr = Output(Bool())
+}
 
-    io.request.ready := 1.B
+class I2c[A <: AbstrRequest, B <: AbstrResponse]
+        (gen:A, gen1:B) extends AbstractDevice {
+    val io = IO(new I2cIO(gen, gen1))
+
+    io.req.ready := 1.B
 
     val i2c_top = Module (new I2C_Top)
 
@@ -23,18 +29,18 @@ class i2c(val req:AbstrRequest, val rsp:AbstrResponse)(implicit val config:BusCo
     val data_reg = Wire(UInt(32.W))
     val addr_reg = Wire(UInt(8.W))
 
-    write_register := Mux(io.request.fire(), io.request.bits.isWrite, false.B)
-    read_register := Mux(io.request.fire(), !io.request.bits.isWrite, false.B)
-    data_reg := io.request.bits.dataRequest
-    addr_reg := io.request.bits.addrRequest(6,0)
+    write_register := Mux(io.req.fire(), io.req.bits.isWrite, false.B)
+    read_register := Mux(io.req.fire(), !io.req.bits.isWrite, false.B)
+    data_reg := io.req.bits.dataRequest
+    addr_reg := io.req.bits.addrRequest(6,0)
     i2c_top.io.wdata := data_reg
     i2c_top.io.addr := addr_reg
     i2c_top.io.we := write_register
     i2c_top.io.ren := read_register
 
-    io.response.bits.dataResponse := RegNext(Mux(io.response.ready , i2c_top.io.wdata , 0.U))
-    io.response.valid := RegNext(Mux(write_register || read_register, true.B, false.B))
-    io.response.bits.error := RegNext(Mux(io.response.ready , i2c_top.io.intr , 0.U))
+    io.rsp.bits.dataResponse := RegNext(Mux(io.rsp.ready , i2c_top.io.wdata , 0.U))
+    io.rsp.valid := RegNext(Mux(write_register || read_register, true.B, false.B))
+    io.rsp.bits.error := RegNext(Mux(io.rsp.ready , i2c_top.io.intr , 0.U))
 
     i2c_top.io.sda_in := io.cio_i2c_sda_in
 
