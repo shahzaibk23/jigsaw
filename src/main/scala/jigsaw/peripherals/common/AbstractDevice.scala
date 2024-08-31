849d0b5ebeb25d44cddc88cc30c340c9adcb7a12
shahzaibk23 <shahzaibceo@gmail.com>
2024-08-31 20:04:50 +0500

    add abstract classes for DeviceAdapter and its IO

---
+++ src/main/scala/jigsaw/peripherals/common/AbstractDevice.scala
@@ -0,0 +1,13 @@
+package jigsaw.peripherals.common
+
+import chisel3._
+import chisel3.util._ 
+
+import caravan.bus.common.{AbstrRequest, AbstrResponse}
+
+abstract class AbstractDeviceIO[A <: AbstrRequest, B <: AbstrResponse] extends Bundle{
+    val req: DecoupledIO[A]
+    val rsp: DecoupledIO[B]
+}
+
+abstract class AbstractDevice extends Module
